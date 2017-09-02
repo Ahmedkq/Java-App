5bff8b0dd757eab673f9a589fa8af62a78fa207f
Ahmedkq <AALQAHTANI4@MAIL.STMARYTX.EDU>
2017-09-01 22:37:42 -0500

    Add files via upload

---
+++ TestShip.java
@@ -0,0 +1,15 @@
+
+public class TestShip {
+
+	public static void main(String[] args) {
+		Ship[] Ship = new Ship[3];
+		Ship[0] = new Ship("Triumph", 1960);
+		Ship[1] = new CruiseShip("Disney Magic", 2400);
+		Ship[2] = new CargoShip("Black Pearl", 50000);
+
+		System.out.println(Ship[0].toString());
+		System.out.println(Ship[1].toString());
+		System.out.println(Ship[2].toString());
+
+	}
+}
