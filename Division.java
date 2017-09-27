bb4b7ab548614b3754d9cd80759e6b541dfe97f6
Ahmedkq <AALQAHTANI4@MAIL.STMARYTX.EDU>
2017-09-26 23:06:18 -0500

    Update Division.java

--- Division.java
+++ Division.java
@@ -2,32 +2,25 @@ import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Division {
-		public static void main(String[] args) {
-			Scanner input = new Scanner(System.in);
-			System.out.print("Enter two integers: ");
+	public static void main(String[] args) {
+		System.out.print("Enter two integers: ");
 
-			
-			
+		try {
+			Scanner input = new Scanner(System.in);
+
 			int number1 = input.nextInt();
 			int number2 = input.nextInt();
-			
-			try{ 
-			
-			System.out.println(number1 + " / " + number2 + " is " + (number1 / number2)); }
-			
-			catch (ArithmeticException e) {
-				System.out.println("Don't Enter a Zero");
-			}
-			
-			catch (InputMismatchException I) {
-				System.out.println("Don't Enter a Latter");
-			}
-			
-			
-	
-			
-			
+
+			System.out.println(number1 + " / " + number2 + " is " + (number1 / number2));
 		}
+
+		catch (ArithmeticException e) {
+			System.out.println("Don't Enter a Zero");
+		}
+
+		catch (InputMismatchException I) {
+			System.out.println("Don't Enter a Latter");
+		}
+
 	}
-
-
+}
